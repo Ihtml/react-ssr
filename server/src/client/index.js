90acf3aa6f3af7a79a50f8e51c9c800dbc4c67e3
Ihtml <2483690020@qq.com>
2019-05-26 21:34:46 +0800

    feat(client and server): client router and server router
    客户端使用BrowserRouter,服务端使用StaticRouter,需传入location和context
    
    BREAKING CHANGE: 引入客户端路由和服务端路由

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -2,8 +2,16 @@
 
 import React from 'react'
 import ReactDom from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+import Routes from '../Routes'
 
-import Home from '../containers/Home'
+const App = () => {
+    return (
+        <BrowserRouter>
+            {Routes}
+        </BrowserRouter>
+    )
+}
 
 // 用hydrate
-ReactDom.hydrate(<Home />, document.getElementById('root'))
+ReactDom.hydrate(<App/>, document.getElementById('root'))
--- server/src/server/index.js
+++ server/src/server/index.js
@@ -1,32 +1,41 @@
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import Home from '../containers/Home'
+// 服务端渲染路由只能用StaticRouter
+import { StaticRouter } from 'react-router-dom'
+import Routes from '../Routes'
 
 const app = express()
 // 只要访问静态文件，就到根目录的public目录下查找
 app.use(express.static('public'))
 
-const content = renderToString(<Home />)
-
 // 访问应用的跟路径，展示一个内容为hello world的html
-app.get('/', (req, res) => res.send(
-	`
-	<html>
-		<head>
-			<title>react ssr</title>
-		</head>
-		<body>
-			<div id="root">${content}</div>
-			<script src='/index.js'></script>
-		</body>
-	</html>
-	`
-))
+app.get('/', (req, res) => {
+    const content = renderToString((
+        // req.path获取用户请求的路径
+        <StaticRouter location={req.path} context={{}}>
+            {Routes}
+        </StaticRouter>
+    ))
+    res.send(
+        `
+        <html>
+            <head>
+                <title>react ssr</title>
+            </head>
+            <body>
+                <div id="root">${content}</div>
+                <script src='/index.js'></script>
+            </body>
+        </html>
+        `
+    )
+}
+)
 
 const server = app.listen(3001, () => {
-	const host = server.address().address
-	const port = server.address().port
+    const host = server.address().address
+    const port = server.address().port
 
-	console.log('Example app listening at http://%s:%s', host, port)
+    console.log('Example app listening at http://%s:%s', host, port)
 })
