49043f014ea5cdbf4103692ab49ce28abf76a2ac
Ihtml <2483690020@qq.com>
2019-05-28 22:29:27 +0800

    feat(client and server): use Link
    用Link标签串联起整个路由流程

--- server/src/containers/Home/index.js
+++ server/src/containers/Home/index.js
@@ -1,10 +1,9 @@
-// node环境下，只能用common.js规范，而不能用esmodile
-// 在使用webpack和babel后，js文件会被编译打包，可以使用esmodule语法了
 import React from 'react'
-
+import Header from '../../components/Header'
 const Home = () => {
     return (
         <div>
+            <Header />
             <div>this is new Home component</div>
             <button onClick={()=>{alert('clicked!')}}>
                 click
--- server/src/containers/Login/index.js
+++ server/src/containers/Login/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <div>this is Login page</div>
         </div>
     )
