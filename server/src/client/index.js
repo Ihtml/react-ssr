4a21b6ee787d6528510945010bd45287dda9f6e6
Ihtml <2483690020@qq.com>
2019-05-29 22:27:22 +0800

    feat(client and server): use redux-thunk

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -4,13 +4,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import Routes from '../Routes'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
 const reducer = (state = {name: 'IFE'}, action) => {
     return state
 }
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 const App = () => {
     return (
--- server/src/server/utils.js
+++ server/src/server/utils.js
@@ -3,15 +3,16 @@ import { renderToString } from 'react-dom/server'
 // 服务端渲染路由只能用StaticRouter
 import { StaticRouter } from 'react-router-dom'
 import Routes from '../Routes'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
 export const render = (req) => {
 
     const reducer = (state = {name: 'IFE'}, action) => {
         return state
     }
-    const store = createStore(reducer)
+    const store = createStore(reducer, applyMiddleware(thunk))
 
     const content = renderToString((
         <Provider store={store}>
