87d727a2ad9cdb865f52def5b49b6c28abb83590
Ihtml <2483690020@qq.com>
2019-05-29 22:21:48 +0800

    feat(server): server store

--- server/src/server/utils.js
+++ server/src/server/utils.js
@@ -3,13 +3,23 @@ import { renderToString } from 'react-dom/server'
 // 服务端渲染路由只能用StaticRouter
 import { StaticRouter } from 'react-router-dom'
 import Routes from '../Routes'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 export const render = (req) => {
+
+    const reducer = (state = {name: 'IFE'}, action) => {
+        return state
+    }
+    const store = createStore(reducer)
+
     const content = renderToString((
-        // req.path获取用户请求的路径
-        <StaticRouter location={req.path} context={{}}>
-            {Routes}
-        </StaticRouter>
+        <Provider store={store}>
+            {/* req.path获取用户请求的路径 */}
+            <StaticRouter location={req.path} context={{}}>
+                {Routes}
+            </StaticRouter>
+        </Provider>
     ))
     return `
     <html>
