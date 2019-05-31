38eb4fcc4ff77596bba441a8c990a707ccb3f96e
Ihtml <2483690020@qq.com>
2019-05-31 21:33:59 +0800

    refactor(client and server): use combineReducers
    构建rudex代码结构,使用combineReducer

---
+++ server/src/containers/Home/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+    name: 'IFE',
+    newsList: []
+}
+
+export default (state = defaultState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
--- server/src/store/index.js
+++ server/src/store/index.js
@@ -1,10 +1,10 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 import thunk from 'redux-thunk'
+import {reducer as homeReducer} from '../containers/Home/store'
 
-const reducer = (state = {name: 'IFE'}, action) => {
-    return state
-}
-
+const reducer = combineReducers({
+    home: homeReducer
+})
 // 每个用户获得不一样的store
 const getStore = () => {
     return createStore(reducer, applyMiddleware(thunk))
