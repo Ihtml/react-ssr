355db44d4fb095d69c49292f6673bf3e7247404a
Ihtml <2483690020@qq.com>
2019-06-16 21:32:31 +0800

    feat(server): dispatch changeLogin action
    getHeaderInfo方法发送请求，获取登录状态的数据，再dispatch派发changeLogin这个action，改变reducer中的数据

--- server/src/components/Header/store/actions.js
+++ server/src/components/Header/store/actions.js
@@ -1,9 +1,15 @@
+import {CHANGE_LOGIN} from './constants'
 
+const changeLogin = (value) => ({
+    type: CHANGE_LOGIN,
+    value
+})
 export const getHeaderInfo = () => {
     // 使用redux-thunk后，如果返回的是函数，会接收dispatch方法做参数
     return (dispatch, getState, axiosInstance) => {
         return axiosInstance.get('/api/isLogin').then((res) => {
-            console.log(res)
+            console.log("res is: ", res)
+            dispatch(changeLogin(res.data.data.login))
         })
     }
 }
