14add6b570a3e63bd35166c31b7ab6535b13d69c
Ihtml <2483690020@qq.com>
2019-06-25 23:58:20 +0800

    feat(server): dispatch translationList
    派发数据

--- server/src/containers/Translation/store/actions.js
+++ server/src/containers/Translation/store/actions.js
@@ -1,3 +1,10 @@
+import {CHANGE_LIST} from './constants'
+
+const changeList = (list) => ({
+    type: CHANGE_LIST,
+    list
+})
+
 export const getTranslationList = () => {
     return (dispatch, getState, axiosInstance) => {
         return axiosInstance.get('/api/translations.json?secret=abcd')
@@ -5,8 +12,10 @@ export const getTranslationList = () => {
                 console.log(res.data)
                 if (res.data.success) {
                     const list = res.data.data
+                    dispatch(changeList(list))
                 } else {
                     const list = []
+                    dispatch(changeList(list))
                 }
             })
     }
--- server/src/containers/Translation/store/reducer.js
+++ server/src/containers/Translation/store/reducer.js
@@ -1,9 +1,15 @@
+export const CHANGE_LIST = 'HOME/CHANGE_LIST'
 const defaultState = {
     translationList: [{id:1,title:"hello"}]
 }
 
 export default (state= defaultState, action) => {
     switch(action.type) {
+        case CHANGE_LIST:
+            return {
+                ...state,
+                translationList: action.list
+            }
         default:
             return state
     }
