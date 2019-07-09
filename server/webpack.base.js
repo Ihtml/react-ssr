bab54d55549f7828a53f718f35402c4aaad50e88
Ihtml <2483690020@qq.com>
2019-07-09 23:59:07 +0800

    feat(client and server): webpack css config
    webpack配置解析css文件,使用模块化和自定义类名

--- server/webpack.base.js
+++ server/webpack.base.js
@@ -12,6 +12,17 @@ module.exports = {
                     }
                 }]]
             }
+        }, {
+            test: /.css?$/,
+            use: ['style-loader', {
+                loader: 'css-loader',
+                options: {
+                    importLoaders: 1, //接下来要执行loader的数目
+                    modules: true, //支持CSS的模块化
+                    // 自定义CSS类名
+                    localIdentName: '[name]_[local]_[hash:base64:5]'
+                }
+            }]
         }]
     }
 }
